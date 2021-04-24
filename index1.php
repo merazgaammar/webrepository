e764171b72790566e61d4b814ff3158e4e71b78e
merazgaammar <merazga_ammar@yahoo.fr>
2021-04-24 12:18:06 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+?>
+<form action="page1.php" method="POST">
+  user name: <input type="text" name="user"> <br>
+  password: <input type="password" name="password"><br>
+    <input type="submit" name="submit">
+</form>
+<br><button>save</button>
+</body>
+</html>
